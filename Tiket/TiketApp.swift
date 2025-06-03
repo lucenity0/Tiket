48b7aad4fc6e4f264935d00438a00248cd738c5a
Nafees S <snafees.cs23@bmsce.ac.in>
2025-06-03 14:00:16 +0530

    Initial Commit

---
+++ Tiket/TiketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiketApp.swift
+//  Tiket
+//
+//  Created by Nafees S on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TiketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TiketTests/TiketTests.swift
@@ -0,0 +1,17 @@
+//
+//  TiketTests.swift
+//  TiketTests
+//
+//  Created by Nafees S on 03/06/25.
+//
+
+import Testing
+@testable import Tiket
+
+struct TiketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
